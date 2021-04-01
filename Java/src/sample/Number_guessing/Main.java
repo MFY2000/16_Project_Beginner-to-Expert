850b59dc0e0424415e3344ad11348543e26a1552
MFY2000 <FA19BSSE0014@MAJU.EDU.PK>
2021-04-01 21:30:02 +0500

    Update Main.java

--- Java/src/sample/Number_guessing/Main.java
+++ Java/src/sample/Number_guessing/Main.java
@@ -1,10 +1,24 @@
 package sample.Number_guessing;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numberGuess,Turn = 0,RealNumber = (1+(int)(100 * Math.random()));
 
-        System.out.println("");
 
+        System.out.println("Taking a number Between 1 to 127.....");
+        System.out.println("Number is store......");
+
+        System.out.println("Enter the number you guess: ");
+        numberGuess = scan.nextInt();
+
+
+        if(numberGuess == RealNumber){
+            System.out.println("  ");
+        }
 
     }
 }
