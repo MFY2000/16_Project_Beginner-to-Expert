f6d1b79e7829175d21505fdc1e7fff738f6c1343
MFY2000 <FA19BSSE0014@MAJU.EDU.PK>
2021-04-02 20:47:53 +0500

    day 2 project 2 come to end

--- Java/src/sample/_2Note_app/NotePad_Function.java
+++ Java/src/sample/_2Note_app/NotePad_Function.java
@@ -1,6 +1,68 @@
 package sample._2Note_app;
 
-public class NotePad_Function {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class NotePad_Function implements Initializable {
+    public TextField Input_feild;
+
+    String path = "C:\\Users\\MF\\Desktop\\16_Project_Beginner-to-Expert\\Resources\\TXT\\";
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            readFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void clear(ActionEvent actionEvent) {
+        Input_feild.setText("");
+    }
+
+    public void readFile() throws IOException {
+        String value = null;
+        File myObj = new File(path+"File.txt");
+        if (myObj.exists()) {
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine())
+                value = myReader.nextLine();
+            myReader.close();
+        }
+
+        Input_feild.setText(value);
+    }
+
+
+    public void Save_File(ActionEvent actionEvent) throws IOException {
+        String Value =  Input_feild.getText();
+
+        File myObj = new File(path+"File.txt");
+        if (myObj.exists()) {
+            FileWriter myWriter = new FileWriter(path+"File.txt");
+            myWriter.write(Value);
+            myWriter.close();
+        }
+
+
+
+    }
+
+
+    public void SaveAS_File(ActionEvent actionEvent) {
+
+
+    }
 }
